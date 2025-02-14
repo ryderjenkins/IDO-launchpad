3a263eee93b5b7ae744bd8c67ffd992fdb20c44b
BitFancy <bitbanana717@gmail.com>
2025-02-15 03:33:35 +0900

    update: remove hover in docintrocard

--- src/app/components/DocIntroCard.tsx
+++ src/app/components/DocIntroCard.tsx
@@ -1,18 +1,26 @@
-import Image from "next/image"
-import MoviePlayIcon from '../../../public/assets/icon/playIcon.png';
-import GalleryIcon from '../../../public/assets/icon/galleryIcon.png';
+import Image from "next/image";
+import MoviePlayIcon from "../../../public/assets/icon/playIcon.png";
+import GalleryIcon from "../../../public/assets/icon/galleryIcon.png";
 
 export default function DocIntroCard({ selectIcon }: { selectIcon: boolean }) {
     return (
-        <div className="xs:w-full w-[92vw] relative hover:bg-[#494949] rounded-[10px] xl:h-[702px] h-doccardheight xs:mt-[71px] mt-[25px]">
+        <div className="xs:w-full w-[92vw] relative rounded-[10px] xl:h-[702px] h-doccardheight xs:mt-[71px] mt-[25px]">
             <div className="w-full h-full bg-[#ffff] flex justify-center items-center rounded-[10px] opacity-25"></div>
             <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                {selectIcon ?
-                    <Image src={MoviePlayIcon} alt="intromovie" className="xl:h-[70px] h-doccardiconheight w-auto" />
-                    :
-                    <Image src={GalleryIcon} alt="intromovie" className="xl:h-[70px] h-doccardiconheight w-auto" />
-                }
+                {selectIcon ? (
+                    <Image
+                        src={MoviePlayIcon}
+                        alt="intromovie"
+                        className="xl:h-[70px] h-doccardiconheight w-auto"
+                    />
+                ) : (
+                    <Image
+                        src={GalleryIcon}
+                        alt="intromovie"
+                        className="xl:h-[70px] h-doccardiconheight w-auto"
+                    />
+                )}
             </div>
         </div>
-    )
+    );
 }
