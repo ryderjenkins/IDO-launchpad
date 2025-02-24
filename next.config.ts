f043b078b8226e5703f7b7eccab62103746b25e1
bitfancy <bitbanana717@gmail.com>
2025-02-24 10:27:33 +0900

    update: banner video file format with webm
    Co-authored-by: sivicvo <sivicvoso@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      type: 'asset/resource', // This will emit the file to the output directory
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
